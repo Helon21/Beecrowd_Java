fbca7ad93d41b069cb25c1471510fa2a3b829ec1
Helon21 <helonbentes51@hotmail.com>
2024-08-11 16:26:44 -0300

    ex023 teste de selecao

---
+++ ex023/TesteSelecao1.java
@@ -0,0 +1,25 @@
+package ex023;
+
+import java.util.Scanner;
+
+public class TesteSelecao1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int A, B, C, D, somaCD, somaAB;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+
+        sc.close();
+
+        if(B > C && D > A && C + D > A + B && C > 0 && D > 0 && A % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+    }
+}
